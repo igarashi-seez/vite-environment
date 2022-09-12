cba64e063f538fe76dcb2c863f8098e4c1dff1dd
igarashi-seez <igarashi@seez.jp>
2022-09-12 11:51:57 +0900

    複数htmlページの自動生成の確認

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
+import path from 'path';
+
 
 const pageData = {
   '/index.html': {
@@ -12,6 +14,66 @@ const pageData = {
     title: 'List Page',
   },
 };
+
+
+
+
+
+
+// HTMLの複数出力を自動化する
+//./src配下のファイル一式を取得
+import fs from 'fs';
+
+//ファイル一覧のパスを取得する関数
+//https://qiita.com/standard-software/items/e37bfd1db0f5ada192cf
+const readSubDirSync = (folderPath) => {
+  let result = [];
+  const readTopDirSync = ((folderPath) => {
+    let items = fs.readdirSync(folderPath);
+    items = items.map((itemName) => {
+      return path.join(folderPath, itemName);
+    });
+    items.forEach((itemPath) => {
+      result.push(itemPath);
+      if (fs.statSync(itemPath).isDirectory()) {
+        readTopDirSync(itemPath);
+        //再帰処理
+      }
+    });
+  });
+  readTopDirSync(folderPath);
+  return result;
+};
+
+const fileNameList = readSubDirSync(resolve(__dirname, './src/'));
+
+//htmlファイルのみ抽出
+const htmlFileList = fileNameList.filter(file => /.html$/.test(file));
+
+//build.rollupOptions.inputに渡すオブジェクトを生成
+const inputFiles = {};
+for (let i = 0; i < htmlFileList.length; i++) {
+  //'src\'以降をぬきだす
+  const file = htmlFileList[i].substr(htmlFileList[i].indexOf('src\\') + 4);;
+  //componentsディレクトリ内のものはとばす
+  if(file.startsWith('components\\')) {
+    continue; 
+  }
+  inputFiles[file.slice(0,-5)] = resolve(__dirname, './src/' + file );
+  /*
+  この形を自動的に作る
+  input:{
+    index: resolve(__dirname, './src/index.html'),
+    list: resolve(__dirname, './src/list.html')
+  }
+  */
+}
+
+
+
+
+
+
 
 
 export default defineConfig({
@@ -39,10 +101,7 @@ export default defineConfig({
         chunkFileNames: 'assets/js/[name].js',
         entryFileNames: 'assets/js/[name].js',
       },
-      input: {
-        index: resolve(__dirname, './src/index.html'),
-        company:resolve(__dirname,'./src/company/index.html')
-      },
+      input: inputFiles,
     },
   },
   plugins: [
